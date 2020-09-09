2d4fa3c9841dc004328bc77f32c03019f367e90e
Cloud11665 <Cloud11665@gmail.com>
2020-09-10 01:30:31 +0200

    specified py versions

--- setup.py
+++ setup.py
@@ -22,7 +22,10 @@ setuptools.setup(
         'console_scripts': ['octagon = Octagon.__init__:main']
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent"
     ],
     python_requires='>=3.6'
