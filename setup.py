614c4795621ac42e6f1f59f37a580406582bcd2a
Cloud11665 <Cloud11665@gmail.com>
2020-09-09 23:45:39 +0200

    v1.0.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readfile(filename):
 
 setuptools.setup(
     name='Octagon',
-    version='1.0.0',
+    version='1.0.5',
     description='How can I find an octagon? How, Elmo? How?',
     long_description=readfile('README.md'),
     long_description_content_type="text/markdown",
